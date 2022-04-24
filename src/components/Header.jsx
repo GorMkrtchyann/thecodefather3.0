965efd40d88d1f1908b5b8be4fea0b88a13ad1fd
GorMkrtchyan <gormkrtchyann@gmail.com>
2022-04-24 11:36:34 +0400

    start project

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header>
+            <div className="container header">
+                <div className="header_text">
+                    <h1><span>The</span> CodeFather</h1>
+                    <p>Սովորիր և զարգացրու ծրագրավորմանդ գիտելիքները մեզ հետ</p>
+                </div>
+                <div className="header_img">
+                    <img src="images/vectors/vector-1.png" alt="" />
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
